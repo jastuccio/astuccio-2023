19145b3b317c9db7d9e5b64cb9114740b0d494d3
Joe Astuccio <joe@astucc.io>
2022-07-28 17:54:19 -0400

    Initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Header from '$lib/components/Header.svelte';
+	import '$lib/styles/style.scss';
+</script>
+
+<Header />
+
+<header>Hi, I'm a header</header>
+<main>
+	<slot />
+</main>
+<footer>footer</footer>
---
+++ src/routes/contact.svelte
@@ -0,0 +1,3 @@
+<h1>Get in touch</h1>
+
+<p><a href="mailto:joe@astucc.io">Email me!</a></p>
