7899921df43cbedd339303fc9968f77160478f73
Joe Astuccio <joe@astucc.io>
2022-07-29 09:01:30 -0400

    Add API for blog posts

---
+++ src/routes/blog/_post.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let title;
+	export let date;
+
+	if (!title) {
+		title = 'This post needs a title';
+	}
+
+	if (!date) {
+		date = '01-01-3333';
+	}
+</script>
+
+<h1>{title}</h1>
+<p>Published: {date}</p>
+
+<slot />
