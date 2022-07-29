30fbd02eb52339c331949226c5057ca2db91b2d5
Joe Astuccio <joe@astucc.io>
2022-07-29 10:53:22 -0400

    Add fade page transitions

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,12 +1,29 @@
+<script context="module">
+	export const load = ({ url }) => {
+		const currentRoute = url.pathname;
+
+		return {
+			props: {
+				currentRoute
+			}
+		};
+	};
+</script>
+
 <script>
 	import Header from '$lib/components/Header.svelte';
 	import '$lib/styles/style.scss';
+	import { fade } from 'svelte/transition';
+
+	export let currentRoute;
 </script>
 
 <Header />
 
 <header>Hi, I'm a header</header>
-<main>
-	<slot />
-</main>
+{#key currentRoute}
+	<main in:fade={{ duration: 150, delay: 150 }} out:fade={{ duration: 150 }}>
+		<slot />
+	</main>
+{/key}
 <footer>footer</footer>
